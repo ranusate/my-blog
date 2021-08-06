d2a432fd7de4b59370856dca2c0e80f6652356ff
Ranus Ate <radianus.ra@mbteknologi.com>
2021-08-06 16:59:50 +0700

    update ui

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-5"> {{$title}}</h1>
+<h1 class="mb-3 text-center"> {{$title}}</h1>
+
+<div class="row justify-content-center mb-3">
+    <div class="col-md-6">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Serach..">
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="submit">Button</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 @if ($posts->count())
 <div class="card mb-3">
@@ -17,16 +28,12 @@
         <a href="/post/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read More..</a>
     </div>
 </div>
-@else
-<p class="text-center fs-4">No post Found</p>
-@endif
-
 <div class="container">
     <div class="row">
         @foreach ($posts->skip(1) as $post )
         <div class="col-md-4 mb-3">
             <div class="card">
-                <div class="positon-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.7);"> <a href='/categories/{{ $post->category->slug}}'  class="text-decoration-none text-white">{{$post->category->name}}</a></div>
+                <div class="positon-absolute px-3 py-2 text-white" style="background-color: rgba(0, 0, 0, 0.7);"> <a href='/categories/{{ $post->category->slug}}' class="text-decoration-none text-white">{{$post->category->name}}</a></div>
                 <img class=" card-img-top" src="https://source.unsplash.com/500x400?{{$post->category->name}}" alt="{{$posts[0]->category->name}}">
                 <div class="card-body">
                     <h5 class="card-title">{{ $post['title'] }}</h5>
@@ -44,4 +51,9 @@
         @endforeach
     </div>
 </div>
+
+@else
+<p class="text-center fs-4">No post Found</p>
+@endif
+
 @endsection
